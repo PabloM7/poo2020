0b7ab613eac0f867084ca1394fbca31bd84c89ca
PabloM7 <50538339+PabloM7@users.noreply.github.com>
2020-03-19 23:59:11 -0500

    Proyecto Poo

---
+++ poo2020/src/proyectopoo/Producto.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectopoo;
+
+/**
+ *
+ * @author Pablo Montaño
+ */
+public class Producto {
+    protected String nombre;
+    protected String medida;
+    protected int cantidad;
+    protected double precio;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the medida
+     */
+    public String getMedida() {
+        return medida;
+    }
+
+    /**
+     * @param medida the medida to set
+     */
+    public void setMedida(String medida) {
+        this.medida = medida;
+    }
+
+    /**
+     * @return the cantidad
+     */
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    /**
+     * @param cantidad the cantidad to set
+     */
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    /**
+     * @return the precio
+     */
+    public double getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+    public Producto(String nombre, String medida, int cantidad, double precio) {
+        this.nombre = nombre;
+        this.medida = medida;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+    
+}
